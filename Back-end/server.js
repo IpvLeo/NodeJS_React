9bc9fec4e59bf74113b924d267d6c74169868230
IpvLeo <leoanarcopunk77@gmail.com>
2024-09-07 19:06:29 -0300

    front ok

--- Back-end/server.js
+++ Back-end/server.js
@@ -1,10 +1,13 @@
 import express, { response } from 'express';
 import { PrismaClient } from '@prisma/client'
+import cors from 'cors';
+
 
 const prisma = new PrismaClient()
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 
 app.post('/usuarios', async (request, response) => {
